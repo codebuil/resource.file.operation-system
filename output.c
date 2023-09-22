0f73e80daf6434d490656f4f5d009d68b8d41e95
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-09-22 22:46:02 +0100

    Add files via upload

---
+++ output.c
@@ -0,0 +1,12 @@
+extern const char txt1[];
+const char txt1[] = (const char *)(0x00060000);
+
+extern const char txt2[];
+const char txt2[] = (const char *)(0x0006000A);
+
+extern const char txt3[];
+const char txt3[] = (const char *)(0x00060013);
+
+extern const char txt4[];
+const char txt4[] = (const char *)(0x00060020);
+
